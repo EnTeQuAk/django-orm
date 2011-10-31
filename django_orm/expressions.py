d605428e20173cff6b797d5c53f5947a73c0d365
Andrei Antoukh <niwi@niwi.be>
2011-10-31 19:31:28 +0100

    ~F() now works with postgresql, mysql and sqlite. Moved to root package.

--- django_orm/expressions.py
+++ django_orm/expressions.py
@@ -3,15 +3,14 @@
 from django.db.models.expressions import F as BaseF
 
 class F(BaseF):
+    _invert = False
+
     def __invert__(self):
         self._invert = True
         return self
 
     def evaluate(self, evaluator, qn, connection):
-        result = evaluator.evaluate_leaf(self, qn, connection)
-        if self._invert and len(result) == 2 and connection.vendor == 'postgresql':
-            result = list(result)
+        result = list(evaluator.evaluate_leaf(self, qn, connection))
+        if self._invert:
             result[0] = "NOT %s" % result[0]
-            result = tuple(result)
-
-        return result
+        return tuple(result)
