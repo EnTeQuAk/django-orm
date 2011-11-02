8911677c75c25561206ae3fab7e5c4151cd2f1e5
Andrei Antoukh <niwi@niwi.be>
2011-11-02 21:21:13 +0100

    Add singleton class.

--- django_orm/utils.py
+++ django_orm/utils.py
@@ -8,3 +8,16 @@ def remove_diacritic(input):
     without any diacritical marks.
     '''
     return unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
+
+
+class Singleton(type):
+    """ Singleton metaclass. """
+
+    def __init__(cls, name, bases, dct):
+        cls.__instance = None
+        type.__init__(cls, name, bases, dct)
+ 
+    def __call__(cls, *args, **kw):
+        if cls.__instance is None:
+            cls.__instance = type.__call__(cls, *args,**kw)
+        return cls.__instance
