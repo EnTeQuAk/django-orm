be58e1e13711f0f2b06559be1e01570177280dab
Andrei Antoukh <niwi@niwi.be>
2011-11-06 16:43:14 +0100

    Implemented correct handlig values and values_list querysets.

--- django_orm/cache/query.py
+++ django_orm/cache/query.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from django.db.models.query import QuerySet, ITER_CHUNK_SIZE
+from django.db.models.query import QuerySet, ValuesQuerySet, ValuesListQuerySet
+from django.db.models.query import ITER_CHUNK_SIZE
 from django.db import backend, connection
 from django.conf import settings
 
@@ -76,7 +77,7 @@ class CachedQuerySetMixIn(object):
 
     def _clone(self, klass=None, **kwargs):
         """ Clone queryset. """
-        qs = QuerySet._clone(self, klass, **kwargs)
+        qs = super(CachedQuerySetMixIn,self)._clone(klass, **kwargs)
         qs.cache_object_enable = self.cache_object_enable
         qs.cache_queryset_enable = self.cache_queryset_enable
         qs.cache_timeout = self.cache_timeout
@@ -91,7 +92,7 @@ class CachedQuerySetMixIn(object):
         # TODO: make this split up large sets of data based on an option
         # and sets the last param, keys, to how many datasets are stored
         # in the cache to regenerate.
-
+        
         keys = tuple(obj.pk for obj in queryset)
         fields = ()
         #if self._select_related:
@@ -179,17 +180,78 @@ class CachedQuerySetMixIn(object):
 
         return super(CachedQuerySetMixIn, self)._result_iter()
 
+
+
+class CachedQuerySet(CachedQuerySetMixIn, QuerySet):
     def _fill_cache(self, num=None):
-        super(CachedQuerySetMixIn, self)._fill_cache(num=num)
+        super(CachedQuerySet, self)._fill_cache(num=num)
         if not self._iter and not self.from_cache and self.cache_queryset_enable:
             qs_prepared_for_cache = self._prepare_queryset_for_cache(self._result_cache)
             cache.set(self.query_key(), qs_prepared_for_cache, self.cache_timeout)
             cache.set_many(dict([(obj.cache_key, obj) \
                 for obj in self._result_cache]), self.cache_timeout)
 
+    def values(self, *fields):
+        return self._clone(klass=CachedValuesQuerySet, setup=True, _fields=fields)
 
-class CachedQuerySet(CachedQuerySetMixIn, QuerySet):
-    """
-    Extends the QuerySet object and caches results via CACHE_BACKEND.
-    """
+    def values_list(self, *fields, **kwargs):
+        flat = kwargs.pop('flat', False)
+        if kwargs:
+            raise TypeError('Unexpected keyword arguments to values_list: %s'
+                % (kwargs.keys(),))
+        if flat and len(fields) > 1:
+            raise TypeError("'flat' is not valid when values_list is called with more than one field.")
+        return self._clone(klass=CachedValuesListQuerySet, setup=True, flat=flat,
+            _fields=fields)
+
+
+class CachedValuesQuerySetMixIn(object):
+    def _prepare_queryset_for_cache(self, queryset):
+        """
+        This is where the magic happens. We need to first see if our result set
+        is in the cache. If it isn't, we need to do the query and set the cache
+        to (ModelClass, (*<pks>,), (*<select_related fields>,), <n keys>).
+        """
+        # TODO: make this split up large sets of data based on an option
+        # and sets the last param, keys, to how many datasets are stored
+        # in the cache to regenerate.
+        return (self.model, queryset, (), 1)
+
+    def _get_queryset_from_cache(self, cache_object):
+        """
+        We transform the cache storage into an actual QuerySet object
+        automagickly handling the keys depth and select_related fields (again,
+        using the recursive methods of CachedQuerySetMixIn).
+        
+        We effectively would just be doing a cache.multi_get(*pks), grabbing
+        the pks for each releation, e.g. user, and then doing a
+        CachedManager.objects.filter() on them. This also then makes that
+        queryset reusable. So the question is, should that queryset have been
+        reusable? It could be invalidated by some other code which we aren't
+        tieing directly into the parent queryset so maybe we can't do the
+        objects.filter() query here and we have to do it internally.
+        """
+        # TODO: make this work for people who have, and who don't have, instance caching
+        model, keys, fields, length = cache_object
+        return keys
+
+    def _fill_cache(self, num=None):
+        super(CachedValuesQuerySetMixIn, self)._fill_cache(num=num)
+        if not self._iter and not self.from_cache and self.cache_queryset_enable:
+            qs_prepared_for_cache = self._prepare_queryset_for_cache(self._result_cache)
+            cache.set(self.query_key(), qs_prepared_for_cache, self.cache_timeout)
+
+
+class CachedValuesQuerySet(CachedValuesQuerySetMixIn, CachedQuerySetMixIn, ValuesQuerySet):
     pass
+
+
+class CachedValuesListQuerySet(CachedValuesQuerySetMixIn, CachedQuerySetMixIn, ValuesListQuerySet):
+    def query_key(self):
+        sql, params = self.query.get_compiler(using=self.db).as_sql()
+        return "%s:queryset:%s:%s:flat=%s" % (
+            CACHE_KEY_PREFIX,
+            self.model.__name__,
+            hashlib.sha1(sql % params).hexdigest(),
+            getattr(self,'flat', False),
+        )
