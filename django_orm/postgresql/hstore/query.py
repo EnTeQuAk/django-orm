e8f1588a2bb29577d5fd29e51b83243d950917bf
Andrei Antoukh <niwi@niwi.be>
2011-11-11 12:09:57 +0100

    Remove unused commented code on postgresql.hstore

--- django_orm/postgresql/hstore/query.py
+++ django_orm/postgresql/hstore/query.py
@@ -14,42 +14,6 @@ except ImportError:
 
 from django_orm.query_utils import select_query, update_query
 
-#def select_query(method):
-#    def selector(self, *args, **params):
-#        query = self.query.clone()
-#        query.default_cols = False
-#        query.clear_select_fields()
-#        return method(self, query, *args, **params)
-#    return selector
-#
-#
-#def update_query(method):
-#    def updater(self, *args, **params):
-#        self._for_write = True
-#        query = method(self, self.query.clone(UpdateQuery), *args, **params)
-#
-#        forced_managed = False
-#        if not transaction.is_managed(using=self.db):
-#            transaction.enter_transaction_management(using=self.db)
-#            forced_managed = True
-#
-#        try:
-#            rows = query.get_compiler(self.db).execute_sql(None)
-#            if forced_managed:
-#                transaction.commit(using=self.db)
-#            else:
-#                transaction.commit_unless_managed(using=self.db)
-#        finally:
-#            if forced_managed:
-#                transaction.leave_transaction_management(using=self.db)
-#
-#        self._result_cache = None
-#        return rows
-#
-#    updater.alters_data = True
-#    return updater
-
-
 class HStoreWhereNode(WhereNode):
     def make_atom(self, child, qn, connection):
         lvalue, lookup_type, value_annot, param = child
