265f5be431a7e77927aa422d743f5cfbb07a230f
Andrei Antoukh <niwi@niwi.be>
2011-11-13 01:57:20 +0100

    Fix indexes creation method with new options implementation.

--- django_orm/backends/postgresql_psycopg2/creation.py
+++ django_orm/backends/postgresql_psycopg2/creation.py
@@ -44,9 +44,12 @@ class DatabaseCreation(BaseDatabaseCreation):
 
     def sql_indexes_for_model(self, model, style):
         output = super(DatabaseCreation, self).sql_indexes_for_model(model, style)
-        options = getattr(model, '_options', {})
-        indexes = options.get('indexes', [])
-
+        
+        meta = getattr(model, '_orm_meta', None)
+        if not meta:
+            return output
+        
+        indexes = getattr(meta, 'indexes', [])
         if not isinstance(indexes, (list, tuple)):
             raise Exception("aditional_indexes must be a list or tuple")
         
